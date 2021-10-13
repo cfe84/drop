5401f3bdb42b09e707d5439bf2ea542f3b5d4870
Charles Feval <cfe84@users.noreply.github.com>
2021-10-13 15:59:55 -0700

    Update index.ts

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 import { DropServer } from "./DropServer";
 
 const dbFile = "testdb.sqlite3"
-const staticFolder = "..\\frontend"
+const staticFolder = process.env.STATIC_CONTENT || "..\\frontend"
 const port = Number.parseInt(process.env.PORT || "8080")
 
 const db = new DropDb(dbFile)
